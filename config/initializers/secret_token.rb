fc695d39653efb5448f05bfab8ce5055878c43af
Eric Guo <eric.guo@sandisk.com>
2013-04-05 15:43:07 +0800

    After run `rails new Icome`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icome::Application.config.secret_token = '1af07fb2991dac29cc1ed9ad8de3838ed085d0c24103b372e1ba0659e6f55c3ad7f7a39928f339091f96a7b6ad1650c11b50ae1d55edab2ef2c8a07128f7790d'
