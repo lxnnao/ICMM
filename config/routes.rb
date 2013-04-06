a87c9451b8514e6cef6e15d4da94997cae8f7d2a
Eric Guo <eric.guo@sandisk.com>
2013-04-06 16:28:36 +0800

    move root route to top as document suggested.
    http://api.rubyonrails.org/classes/ActionDispatch/Routing/Mapper/Base.html#method-i-root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Icome::Application.routes.draw do
+  root :to => 'homes#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -45,10 +47,6 @@ Icome::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'homes#index'
 
   # See how all your routes lay out with "rake routes"
 
