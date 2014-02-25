60a8b884d2f0b05d6bace9c1028fa74eed83ef64
Carl Liu <carl.liu@sandisk.com>
2014-02-25 19:55:13 +0800

    enable login via badge and same the email if exist.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
     render :text => exception, :status => 500
   end
   protect_from_forgery
+
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protercted
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:badge, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :badge, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:badge, :email, :password, :password_confirmation, :current_password) }
+
+  end
+
 end
