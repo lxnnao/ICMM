647d2901615c0944d93822538b27c8e650862fab
Carl Liu <carl.liu@sandisk.com>
2014-02-25 14:30:30 +0800

    rails generate devise:install

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Icome::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  # require by devise
+  config.action_mailer.default_url_options = { :host => 'blog.cloud-mes.com' }
 end
