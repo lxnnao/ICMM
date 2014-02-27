4517bd4f5fce4cdce427f08e746f88f5c378466c
Carl Liu <carl.liu@sandisk.com>
2014-02-27 13:00:52 +0800

    ADD MODEL company activity and related view

---
+++ app/controllers/company_activitys_controller.rb
@@ -0,0 +1,81 @@
+class CompanyactivitysController < ApplicationController
+  def index
+  	@activitys=CompanyActivity.all
+  	render 'activities_show'
+  end
+
+  # GET /companyactivitys/1
+  # GET /companyactivitys/1.json
+  def show
+    @activitys = CompanyActivity.find(params[:id])
+    @last_name = cookies[:last_name]
+    unless @last_name == nil
+      @name = cookies[:name]
+    end
+    @names = session[:names]
+
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @activitys }
+    end
+  end
+  # GET /companyactivitys/new
+  def new
+    @activity = CompanyActivity.new
+  end
+
+  def edit
+  end
+
+  # POST /companyactivitys
+  # POST /companyactivitys.json
+  def create
+    @companyactivity = CompanyActivity.new(activity_params)
+
+    respond_to do |format|
+      if @companyactivity.save
+        format.html { redirect_to @companyactivity, notice: 'CompanyActivity was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @companyactivity }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @companyactivity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /companyactivitys/1
+  # PATCH/PUT /companyactivitys/1.json
+  def update
+    respond_to do |format|
+      if @companyactivity.update(activity_params)
+        format.html { redirect_to @companyactivity, notice: 'CompanyActivity was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @companyactivity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  # DELETE /companyactivitys/1
+  # DELETE /companyactivitys/1.json
+  def destroy
+    @companyactivity.destroy
+    respond_to do |format|
+      format.html { redirect_to companyactivity_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_activity
+      @companyactivity = CompanyActivity.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def activity_params
+      params.require(:company_activity).permit(:action_eng_name, :action_name)
+    end
+
+end
---
+++ app/models/company_activity.rb
@@ -0,0 +1,9 @@
+class CompanyActivity < ActiveRecord::Base
+  self.table_name = 'company_action'
+  self.primary_key = 'action_sys_id'
+#  belongs_to :binslimit,:primary_key => 'binslimitsid', :foreign_key => "binslimitsid"
+	establish_connection("eic_dev")
+  def readonly?
+  	true
+  end
+end
