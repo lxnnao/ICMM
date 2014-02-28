691816e1cf39382bbc05facd4a0b72be8ec41dea
Carl Liu <carl.liu@sandisk.com>
2014-02-28 16:38:58 +0800

    cancel readonly attribute for the database

--- app/models/company_activity.rb
+++ app/models/company_activity.rb
@@ -3,7 +3,4 @@ class CompanyActivity < ActiveRecord::Base
   self.primary_key = 'action_sys_id'
 #  belongs_to :binslimit,:primary_key => 'binslimitsid', :foreign_key => "binslimitsid"
 	establish_connection("eic_dev")
-  def readonly?
-  	true
-  end
 end
