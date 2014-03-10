4543c044f5c17c83d6460221d1b6ab3a26bb78c5
Carl Liu <carl.liu@sandisk.com>
2014-03-10 14:33:08 +0800

    user table database migrate: from sqlite3 to oracle;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  establish_connection("eic_dev")
   devise :ldap_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def email_required?
     false
+  end
+  before_create do
+    self.id = self.connection.execute("select EIC.EMP_SYS_ID_SEQ.nextval id from dual").fetch[0].round
   end
 
  	before_save :get_ldap_email
