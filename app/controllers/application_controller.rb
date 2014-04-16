f04ce6336824306519a777b63e53ed30d9c45529
Carl Liu <carl.liu@sandisk.com>
2014-04-16 16:30:44 +0800

    I18n session setting for the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  #layout "base"
+  before_filter :set_locale
+
   rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
     render :text => exception, :status => 500
   end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
   protect_from_forgery
 
 	before_filter :configure_permitted_parameters, if: :devise_controller?
@@ -13,5 +20,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:badge, :email, :password, :password_confirmation, :current_password) }
 
   end
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
 
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
