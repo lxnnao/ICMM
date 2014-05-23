b814570fa58e9641aecbe2781ce159770aea04c1
Carl Liu <carl.liu@sandisk.com>
2014-05-23 20:48:11 +0800

    1. answer view can show  select list 2. add delete link for some survey

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,7 @@ class Answer < ActiveRecord::Base
   	self.table_name = 'im_answer'
   	self.primary_key = 'answer_id'
 	belongs_to :question,:class_name => 'Question' ,:primary_key => 'question_id', :foreign_key => "question_id"
-
+  	CONTENT_TYPE=['List','Text']
   before_create do
     self.answer_id = self.connection.execute("select EIC.im_answer_id_SEQ.nextval id from dual").fetch[0].round
   end
